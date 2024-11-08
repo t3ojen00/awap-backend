2174817ff6e1c0ffe134f31e3f93f249167a8118
enniemilia <130854866+enniemilia@users.noreply.github.com>
2024-11-08 12:41:05 +0200

    genre

---
+++ server/dto/GenreClass.js
@@ -0,0 +1,11 @@
+//import movies
+
+//const name = new Movie() (uncomment when movie class available)
+
+class Genre {
+    constructor(name) {
+        this.name = name;
+        this.movies = [];
+    }
+
+}

7fccf1454bbf7eebe299cd15ba740189ae603cdf
enniemilia <130854866+enniemilia@users.noreply.github.com>
2024-12-01 17:26:22 +0200

    sql

--- server/database/web_app.sql
+++ server/database/web_app.sql
@@ -71,13 +71,25 @@ CREATE TABLE GroupMemberships (
 );
 
 -- Reviews table with foreign keys to Movies and Users
-CREATE TABLE Reviews (
+CREATE TABLE movie_reviews (
     review_id SERIAL PRIMARY KEY,
-    movie_id INT REFERENCES Movies(movie_id) ON DELETE CASCADE,  -- Foreign key to Movies
-    user_id INT REFERENCES Users(user_id) ON DELETE CASCADE,     -- Foreign key to Users
-    rating INT CHECK (rating >= 1 AND rating <= 5),              -- Rating between 1 and 5
-    review_text TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    review TEXT NOT NULL,
+    movie_id INT NOT NULL,
+    user_id INT NOT NULL,
+    movie_name VARCHAR(255),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    UNIQUE (movie_id, user_id)
+);
+
+CREATE TABLE movie_rating (
+    rating_id SERIAL PRIMARY KEY,
+    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    movie_id INT NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    UNIQUE (movie_id, user_id)
 );
 
 -- Favorites table with composite key to handle many-to-many relationship between Users and Movies
@@ -85,4 +97,13 @@ CREATE TABLE Favorites (
     user_id INT REFERENCES Users(user_id) ON DELETE CASCADE,     -- Foreign key to Users
     movie_id INT REFERENCES Movies(movie_id) ON DELETE CASCADE,  -- Foreign key to Movies
     PRIMARY KEY (user_id, movie_id)  -- Composite primary key to avoid duplicate favorite entries
+);
+
+CREATE TABLE messages (
+    message_id SERIAL PRIMARY KEY,
+    message TEXT NOT NULL,
+    group_id INT NOT NULL,
+    user_id INT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );

3b8e07d5f5cec6de7083de98cc148b114051f1f7
Ha Nguyen <c3ngth01@student.oamk.fi>
2024-12-05 07:30:06 +0200

    update grouproute/autotest/signin

--- server/index.test.js
+++ server/index.test.js
@@ -1,184 +1,182 @@
 const { expect } = require("chai");
-const axios = require("axios");
-const { insertTestUser, getToken } = require("./config/test.js");
+const fetch = require("node-fetch");
+const { insertTestUser, deleteTestUser, getToken } = require("./config/test");
 
-const base_url = "http://localhost:3000/";
+const base_url = process.env.BASE_URL || "http://localhost:3000/";
 
 describe("POST /registration", () => {
-  const email = "test_registration@example.com";
-  const password = "Test123";
+  describe("POST /users/registration", () => {
+    const email = "test_registration@example.com";
+    const user_name = "test_registration";
+    const password = "Test123";
 
-  it("should register with valid email and password", async () => {
-    const response = await fetch(base_url + "users/registration", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: email, password: password }),
+    // after(async () => {
+    //   await deleteTestUser(email);
+    // });
+
+    it("should register with valid email and password", async () => {
+      const response = await fetch(base_url + "users/registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          user_name,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+      expect(response.status).to.equal(201);
+      expect(data).to.be.an("object");
+      expect(data).to.have.property("user_id").that.is.a("number");
     });
 
-    const data = await response.json();
+    it("should not register a user with a password that does not meet complexity requirements", async () => {
+      const weakPassword = "password";
+      const response = await fetch(base_url + "users/registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "weak_password@example.com",
+          user_name: "weak_password",
+          password: weakPassword,
+        }),
+      });
 
-    expect(response.status).to.equal(200);
-    expect(data).to.be.an("object");
-    expect(data).to.have.property("id");
-    expect(data.id).to.be.a("number");
+      const data = await response.json();
+      expect(response.status).to.equal(400);
+      expect(data)
+        .to.have.property("error")
+        .that.equals(
+          "Password must contain at least one capital letter and one number."
+        );
+    });
   });
 
-  it("should not register a user with a password that does not meet complexity requirements", async () => {
-    const invalidPasswordEmail = "test_invalid@example.com";
-    const invalidPassword = "invalidpassword";
+  describe("POST /users/login", () => {
+    const email = "test_login@example.com";
+    const user_name = "test_login";
+    const password = "Test123";
 
-    const response = await fetch(base_url + "users/registration", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: invalidPasswordEmail,
-        password: invalidPassword,
-      }),
+    insertTestUser(email, user_name, password);
+
+    it("should login with valid credentials", async () => {
+      const response = await fetch(base_url + "users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      expect(response.status).to.equal(200);
+      expect(data).to.be.an("object");
+      expect(data).to.include.all.keys("userId", "email", "token");
+      expect(data.email).to.equal(email);
+    });
+  });
+
+  describe("POST /users/logout", () => {
+    const email = "test_logout@example.com";
+    const user_name = "test_logout";
+    const password = "Test123";
+
+    insertTestUser(email, user_name, password);
+    const token = `Bearer ${getToken(email)}`;
+
+    it("should log out successfully with a valid token", async () => {
+      const response = await fetch(base_url + "users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token, // Token now has the correct format
+        },
+      });
+
+      const data = await response.json();
+      expect(response.status).to.equal(200);
+      expect(data).to.be.an("object");
+      expect(data)
+        .to.have.property("message")
+        .that.equals("Successfully logged out");
     });
 
-    const data = await response.json();
+    it("should fail to log out with an invalid token", async () => {
+      const response = await fetch(base_url + "users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer invalidtoken",
+        },
+      });
 
-    expect(response.status).to.equal(400);
-
-    expect(data).to.be.an("object");
-    expect(data).to.have.property("error");
-    expect(data.error).to.equal(
-      "Password must contain at least one capital letter and one number."
-    );
+      const data = await response.json();
+      expect(response.status).to.equal(403);
+      expect(data).to.have.property("message").that.equals("Invalid token.");
+    });
   });
-});
 
-describe("POST /users/login", () => {
-  const email = "test_login@example.com";
-  const password = "Test123";
+  describe("DELETE /users/delete/:userId", () => {
+    const email = "test_delete@example.com";
+    const user_name = "test_delete";
+    const password = "Test123";
 
-  // Ensure the test user is inserted before tests
-  insertTestUser(email, password);
+    let token;
+    let userId;
 
-  // before(async () => {
-  //   insertTestUser(email, password);
-  // });
-
-  it("should login with valid credentials", async () => {
-    const response = await fetch(base_url + "users/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
+    insertTestUser(email, user_name, password).then((id) => {
+      userId = id; // Set the userId
+      if (!userId) {
+        throw new Error(`User not found for email: ${email}`);
+      }
+      // Set token
+      token = `Bearer ${getToken(email)}`;
     });
 
-    const data = await response.json();
-    expect(response.status).to.equal(200);
-    expect(data).to.be.an("object");
-    expect(data).to.include.all.keys("userId", "email", "token");
-    expect(data.email).to.equal(email);
-  });
-});
-
-describe("POST /users/logout", () => {
-  const email = "test_logout@example.com";
-  const password = "Test123";
-
-  insertTestUser(email, password);
-  const token = `Bearer ${getToken(email)}`;
-
-  // let token;
-  // before(async () => {
-  //   insertTestUser(email, password);
-  //   token = `Bearer ${getToken(email)}`;
-  // });
-
-  it("should log out successfully with a valid token", async () => {
-    const response = await fetch(base_url + "users/logout", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token, // Token now has the correct format
-      },
+    it("should delete the authenticated user", async () => {
+      const response = await fetch(`${base_url}users/delete/${userId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+      const data = await response.json();
+      expect(response.status).to.equal(200);
+      expect(data)
+        .to.have.property("message")
+        .that.equals("User deleted successfully");
     });
 
-    const data = await response.json();
-    expect(response.status).to.equal(200);
-    expect(data).to.be.an("object");
-    expect(data)
-      .to.have.property("message")
-      .that.equals("Successfully logged out");
-  });
-
-  it("should fail to log out with an invalid token", async () => {
-    const response = await fetch(base_url + "users/logout", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer invalidtoken",
-      },
+    it("should not delete a user with SQL injection", async () => {
+      const response = await fetch(base_url + "users/delete/id=0 or id > 0", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+      const data = await response.json();
+      expect(response.status).to.equal(400);
+      expect(data).to.have.property("message").that.equals("Invalid user ID.");
     });
 
-    const data = await response.json();
-    expect(response.status).to.equal(403);
-    expect(data).to.have.property("message").that.equals("Invalid token.");
-  });
-});
-
-describe("DELETE /users/delete/:userId", () => {
-  const email = "test_delete22@example.com";
-  const password = "Test123";
-
-  let token;
-  let userId;
-
-  insertTestUser(email, password).then((id) => {
-    userId = id; // Set the userId
-    if (!userId) {
-      throw new Error(`User not found for email: ${email}`);
-    }
-    // Set token
-    token = `Bearer ${getToken(email)}`;
-  });
-
-  it("should delete the authenticated user", async () => {
-    const response = await fetch(`${base_url}users/delete/${userId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
-      },
+    it("should fail to delete with an invalid token", async () => {
+      const response = await fetch(base_url + "users/delete/1", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer invalidtoken", // Invalid token
+        },
+      });
+      const data = await response.json();
+      expect(response.status).to.equal(403);
+      expect(data).to.have.property("message").that.equals("Invalid token.");
     });
-    const data = await response.json();
-    expect(response.status).to.equal(200);
-    expect(data)
-      .to.have.property("message")
-      .that.equals("User deleted successfully");
-  });
-
-  it("should not delete a user with SQL injection", async () => {
-    const response = await fetch(base_url + "users/delete/id=0 or id > 0", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
-      },
-    });
-    const data = await response.json();
-    expect(response.status).to.equal(400);
-    expect(data).to.have.property("message").that.equals("Invalid user ID.");
-  });
-
-  it("should fail to delete with an invalid token", async () => {
-    const response = await fetch(base_url + "users/delete/1", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer invalidtoken", // Invalid token
-      },
-    });
-    const data = await response.json();
-    expect(response.status).to.equal(403);
-    expect(data).to.have.property("message").that.equals("Invalid token.");
   });
 });
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -41,40 +41,99 @@ userRouter.get("/:user_id", authToken, async (req, res) => {
 });
 
 //registeration to the site
+// userRouter.post("/registration", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     if (!email || !password) {
+//       return res.status(400).send({ error: "Email and password are required" });
+//     }
+//     //query to check if the email already exists
+
+//     const existingEmail = "SELECT * FROM Users WHERE email = $1";
+//     const { rows } = await pool.query(existingEmail, [email]);
+
+//     if (rows.length > 0) {
+//       return res.status(400).json({ error: "Email already exists" });
+//     }
+
+//     // Password need least one capital letter and one number-H add
+//     const passwordRequried = /^(?=.*[A-Z])(?=.*\d).+$/;
+//     if (!passwordRequried.test(password)) {
+//       return res.status(400).json({
+//         error:
+//           "Password must contain at least one capital letter and one number.",
+//       });
+//     }
+
+//     const hash = await bcrypt.hash(password, 10);
+
+//     const sql =
+//       "INSERT INTO Users (email, password_hash) VALUES ($1, $2) RETURNING user_id";
+//     const result = await pool.query(sql, [email, hash]);
+
+//     res.status(200).json({ id: result.rows[0].user_id });
+//   } catch (error) {
+//     console.error("Error during registration:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+//add user_name
 userRouter.post("/registration", async (req, res) => {
   try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.status(400).send({ error: "Email and password are required" });
+    const { email, user_name, password } = req.body;
+    if (!email || !user_name || !password) {
+      return res
+        .status(400)
+        .json({ error: "Email, username, and password are required." });
     }
-    //query to check if the email already exists
-
-    const existingEmail = "SELECT * FROM Users WHERE email = $1";
-    const { rows } = await pool.query(existingEmail, [email]);
-
-    if (rows.length > 0) {
-      return res.status(400).json({ error: "Email already exists" });
+    // Check if email or username already exists
+    const userCheckQuery =
+      "SELECT email, user_name FROM Users WHERE email = $1 OR user_name = $2";
+    const { rows: existingUsers } = await pool.query(userCheckQuery, [
+      email,
+      user_name,
+    ]);
+    if (existingUsers.length > 0) {
+      const conflict = existingUsers[0];
+      if (conflict.email === email) {
+        return res.status(400).json({ error: "Email already exists." });
+      }
+      if (conflict.user_name === user_name) {
+        return res.status(400).json({ error: "Username already exists." });
+      }
     }
-
-    // Password need least one capital letter and one number-H add
-    const passwordRequried = /^(?=.*[A-Z])(?=.*\d).+$/;
-    if (!passwordRequried.test(password)) {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d).+$/;
+    if (!passwordRegex.test(password)) {
       return res.status(400).json({
         error:
           "Password must contain at least one capital letter and one number.",
       });
     }
-
-    const hash = await bcrypt.hash(password, 10);
-
-    const sql =
-      "INSERT INTO Users (email, password_hash) VALUES ($1, $2) RETURNING user_id";
-    const result = await pool.query(sql, [email, hash]);
-
-    res.status(200).json({ id: result.rows[0].user_id });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const insertUserQuery =
+      "INSERT INTO Users (email, user_name, password_hash) VALUES ($1, $2, $3) RETURNING user_id";
+    const { rows: newUser } = await pool.query(insertUserQuery, [
+      email,
+      user_name,
+      hashedPassword,
+    ]);
+    // Return success response
+    res.status(201).json({
+      message: "User registered successfully.",
+      user_id: newUser[0].user_id,
+    });
   } catch (error) {
     console.error("Error during registration:", error);
+
+    if (error.detail.includes("email")) {
+      return res.status(400).json({ error: "Email already exists." });
+    }
+    if (error.detail.includes("user_name")) {
+      return res.status(400).json({ error: "Username already exists." });
+    }
+
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
@@ -142,7 +201,7 @@ userRouter.post("/login", async (req, res) => {
     );
 
     // Send the token to the client
-    res.json({
+    res.status(200).json({
       message: "Login successful",
       token,
       userId: user.user_id,
